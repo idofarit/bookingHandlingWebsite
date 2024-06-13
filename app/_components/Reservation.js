81e0696036c5dbe4ebeb421a352d8360528fa8ab
idofarit <idofarit@gmail.com>
2024-06-14 02:48:59 +0530

    auth flow prog

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,17 +1,25 @@
-import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import { auth } from "../_lib/auth";
+import { getBookedDatesByCarId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 async function Reservation({ car }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
-    getBookedDatesByCabinId(car.id),
+    getBookedDatesByCarId(car.id),
   ]);
+
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector settings={settings} bookedDates={bookedDates} car={car} />
-      <ReservationForm car={car} />
+      {session?.user ? (
+        <ReservationForm car={car} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
---
+++ app/_lib/auth.js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { createCustomer, getCustomer } from "./data-service";
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingCustomer = await getCustomer(user.email);
+
+        if (!existingCustomer) {
+          await createCustomer({ email: user.email, fullName: user.name });
+        }
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const customer = await getCustomer(session.user.email);
+      session.user.customerId = customer.id;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export const {
+  signIn,
+  signOut,
+  auth,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
